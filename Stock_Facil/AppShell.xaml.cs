7f37daa088195f6dc86edf2e677ef85f16f746c6
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2026-06-26 17:21:02 -0400

    Primeros pasos del proyecto Stock Fácil
    Creación del proyecto .NET MAUI

---
+++ Stock_Facil/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Stock_Facil
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Stock_Facil/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Stock_Facil")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Stock_Facil.Pages")]
